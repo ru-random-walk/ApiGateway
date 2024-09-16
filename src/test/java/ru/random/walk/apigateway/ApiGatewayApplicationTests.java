bc73fd0e1d6b446a267a4f79f48e4e3d9b369fcd
Nikita Kuritsyn <martyz89@yandex.ru>
2024-09-16 12:45:15 +0300

    Create gateway

---
+++ src/test/java/ru/random/walk/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.random.walk.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
